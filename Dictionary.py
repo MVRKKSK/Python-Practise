9e4a12ebb48b1d80b213e39dbee4a003d3ea0698
MVRKKSK <miryala.kautilya2020@vitbhopal.ac.in>
2022-02-07 23:16:18 +0530

    added dictonaries and lists

---
+++ Dictionary.py
@@ -0,0 +1,5 @@
+Dmart_products = {"apples":5.54 , "mangoes": 5.168 , "oranges": 1.546}
+print(Dmart_products["apples"])
+
+anime_rating = {"naruto":8.6 , "attack on titan": 9.0 , "full metal alchemist": 9.1 , "death note": 9.1 , "one piece": 8.8 }
+print(anime_rating["naruto"])
